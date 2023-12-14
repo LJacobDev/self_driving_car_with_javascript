747279342aa3e134c071d305261bd302b7ee4806
LJacobDev <LJacobDev@gmail.com>
2023-12-14 00:32:32 -0500

    add basic left right up down motion

---
+++ controls.js
@@ -0,0 +1,62 @@
+class Controls
+{
+
+    constructor()
+    {
+        this.forward = false;
+        this.reverse = false;
+        this.left = false;
+        this.right = false;
+
+        this.#addKeyboardListeners();
+        
+        //counter shows that onkeydown event
+        //fires many times per second and not just once
+        //whenever holding it down after one press
+        //this.counter = 0;
+    }
+
+    #addKeyboardListeners()
+    {
+        document.onkeydown = (event) => {
+            
+            switch(event.key){
+                case "ArrowLeft":
+                    this.left = true;
+                    break;
+                case "ArrowRight":
+                    this.right = true;
+                    break;
+                case "ArrowUp":
+                    this.forward = true;
+                    break;
+                case "ArrowDown":
+                    this.reverse = true;
+                    break;
+            }
+            //console.log(this.counter++)
+            console.table(this);
+        }
+
+        document.onkeyup = (event) => {
+            
+            switch(event.key){
+                case "ArrowLeft":
+                    this.left = false;
+                    break;
+                case "ArrowRight":
+                    this.right = false;
+                    break;
+                case "ArrowUp":
+                    this.forward = false;
+                    break;
+                case "ArrowDown":
+                    this.reverse = false;
+                    break;
+            }
+            //console.log(this.counter++);
+            console.table(this);
+        }
+
+    }
+}
--- main.js
+++ main.js
@@ -1,9 +1,30 @@
 const canvas = document.getElementById("myCanvas");
-canvas.height = window.innerHeight;
 canvas.width = 200;
 
 
 const context = canvas.getContext("2d");
 
 const car = new Car(100,100,30,50);
-car.draw(context);
+
+animate();
+
+function animate(){
+
+    //putting this in the animation loop
+    //allows it to vertically resize to window changes
+    //but it also acts like erasing the
+    //past locations of the car so it doesn't leave streaks
+    //when it moves
+    canvas.height = window.innerHeight;
+
+
+    //gets movement and updates xy position
+    car.update();
+
+    //draw car on canvas at new positoin
+    car.draw(context);
+
+    //this will cause this animate method 
+    //to be run several times per second
+    requestAnimationFrame(animate);
+}
