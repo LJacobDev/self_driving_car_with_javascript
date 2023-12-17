97fb77311d00c0fb5cf6a67eba1b76f4d6e3c93a
LJacobDev <LJacobDev@gmail.com>
2023-12-16 22:00:07 -0500

    added more realistic turning controls

--- car.js
+++ car.js
@@ -13,6 +13,9 @@ class Car
         this.maxSpeed = 3;
         this.friction = 0.05;
 
+        this.angle = 0;
+        this.turnSpeed = 0.03;
+
         this.controls = new Controls();
     }
 
@@ -47,36 +50,89 @@ class Car
             this.speed = 0;
         }
         
-        //console.log(this.speed);
-        this.y-= this.speed;
-
 
         //implement turning
         //starting point with a simple component,
         //then improve it to have better handling next
-        if(this.controls.left){
-            this.x-=2;
-        }
-        if(this.controls.right){
-            this.x+=2;
+        if(this.speed != 0){
+
+            /*
+                this was my first try of correcting
+                the steering when going forward vs backward
+
+            if(this.controls.left){
+                if(this.speed > 0)
+                    this.angle += this.turnSpeed;
+                if(this.speed < 0)
+                    this.angle -= this.turnSpeed;
+            }
+            if(this.controls.right){
+                if(this.speed > 0)
+                    this.angle -= this.turnSpeed;
+                if(this.speed < 0)
+                    this.angle += this.turnSpeed;
+                
+            }
+            */
+
+            //this is the one done in the tutorial:
+            const flip = this.speed > 0 ? 1: -1;
+
+            if(this.controls.left){
+                this.angle += this.turnSpeed * flip;
+            }
+            if(this.controls.right){
+                this.angle -= this.turnSpeed * flip;
+            }
+
         }
 
-        
+
+
+        //console.log(this.speed);
+        //this.y-= this.speed;
+
+        //getting sin/cos on a unit circle gets direction
+        //relative to centre point
+        //scaling it by speed makes it a velocity vector
+        //because the unit circle is rotated 90 degrees,
+        //and 0 is pointing as car's forward,
+        //sine is used to get the x motion quantity
+        //and cosine is used to get y motion quantity
+        //then both are scaled by speed to apply
+        //rotation oriented velocity 
+    
+        this.x -= Math.sin(this.angle) * this.speed;
+        this.y -= Math.cos(this.angle) * this.speed;
+       
     }
 
     draw(context)
     {
-        //console.log("draw method running");
-        //console.log(context);
-
+        //this was added along with context.restore()
+        //but the effect it has isn't immediately visible
+        //at this stage of just adding the .translate()
+        //and .rotate()
+        context.save();
+        
+        //apply rotation and position
+        context.translate(this.x, this.y);
+        context.rotate(-this.angle);
+        
         context.beginPath();
         context.rect(
-            this.x - this.width / 2,
-            this.y - this.height / 2,
+            -this.width / 2,
+            -this.height / 2,
             this.width,
             this.height
         );
         context.fill();
+
+        //this was added along with context.save()
+        //but the effect it has isn't immediately visible
+        //at this stage of just adding the .translate()
+        //and .rotate()
+        context.restore();
 
     }
 }
