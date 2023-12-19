324b1a1885c53feb7393c23d19e762d775bd31cc
LJacobDev <LJacobDev@gmail.com>
2023-12-19 01:10:48 -0500

    add road movement relative to car movement

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const context = canvas.getContext("2d");
 
 const road = new Road(canvas.width/2, canvas.width * 0.9, 3);
 
-const car = new Car(100,100,30,50);
+const car = new Car(road.getLaneCenter(1),100,30,50);
 
 animate();
 
@@ -19,13 +19,27 @@ function animate(){
     canvas.height = window.innerHeight;
 
 
-    //gets movement and updates xy position
+    //gets car movement and updates its xy position
     car.update();
+
+    
+    //make the road move the opposite vertical
+    //way that the car moves
+    //making the car stay in view and the road appear to go by
+    context.save();
+    
+    //the canvas moves opposite direction of the car,
+    //but is translated so that the car appears to be
+    //70% of the way down the canvas viewspace
+    context.translate(0, -car.y + canvas.height * 0.7);
 
     road.draw(context);
 
-    //draw car on canvas at new positoin
+    //draw car on canvas at new position
     car.draw(context);
+
+
+    context.restore();
 
     //this will cause this animate method 
     //to be run several times per second
--- road.js
+++ road.js
@@ -16,6 +16,30 @@
         const infinity = 1000000;
         this.topEdge = -infinity;
         this.bottomEdge = infinity;
+
+        //define borders of the road
+        //using arrays for flexibility
+        //Initially will use 4 x,y points,
+        //but adding curved roads and borders
+        //with additional points is possible
+        const topLeft = {x:this.leftEdge, y:this.topEdge};
+        const bottomLeft = {x:this.leftEdge, y:this.bottomEdge};
+        const topRight = {x:this.rightEdge, y:this.topEdge};
+        const bottomRight = {x:this.rightEdge, y:this.bottomEdge};
+        this.borders = [
+            [topLeft, bottomLeft],
+            [topRight, bottomRight]
+        ];
+    }
+
+    getLaneCenter(laneIndex)
+    {
+        const laneWidth = this.width / this.laneCount;
+        //this starts on the left edge, adds half a lane width,
+        //to be in centre of left lane, then adds the width
+        //of a lane multiplied by the number of lanes over
+        //being indexed, getting centre x of any given lane
+        return this.leftEdge + laneWidth/2 + laneIndex * laneWidth;
     }
 
     draw(context)
@@ -25,7 +49,7 @@
 
         //to get the lane marking x positions
         //interpolate between left and right x values
-        for(let i=0; i <= this.laneCount; i++)
+        for(let i=1; i <= this.laneCount-1; i++)
         {
             const x = lerp(
                 this.leftEdge,
@@ -35,25 +59,26 @@
 
             //add dashed line styling to the middle lane markers
             //but not to the left and right edge markers
-            if(i > 0 && i < this.laneCount)
-            {
-                //20 pixel dash with 20 pixel gaps
-                context.setLineDash([20,20]);
-            }
-            else
-            {
-                //giving an empty array makes a solid line
-                context.setLineDash([]);
-            }
 
+            //20 pixel dash with 20 pixel gaps
+            context.setLineDash([20,20]);
+            
             context.beginPath();
             context.moveTo(x, this.topEdge);
             context.lineTo(x, this.bottomEdge);
             context.stroke();
-        
-
+            
         }
-
+            
+        
+        //giving an empty array makes a solid line
+        context.setLineDash([]);
+        this.borders.forEach(border=>{
+            context.beginPath();
+            context.moveTo(border[0].x, border[0].y);
+            context.lineTo(border[1].x, border[1].y);
+            context.stroke();
+        })
     }
  }
 
